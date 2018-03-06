f3668efdfa99b100261c81f92c48cb441d994d87
scottwombat <scott.wombat@gmail.com>
2018-03-07 10:00:41 +1100

    init

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/static", express.static(path.join(__dirname, "/public/compiled")));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mygraphql-app.herokuapp.com/graphql');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
