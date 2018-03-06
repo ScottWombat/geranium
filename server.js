28048c11170fddb4cf41c988be3f982b673a24f1
scottwombat <scott.wombat@gmail.com>
2018-03-07 09:57:07 +1100

    init

--- server.js
+++ server.js
@@ -19,6 +19,24 @@ app.use("/static", express.static(path.join(__dirname, "/public/ico")));
 app.use("/static", express.static(path.join(__dirname, "/../public/js")));
 app.use("/static", express.static(path.join(__dirname, "/public/compiled")));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+   });
 
 app.get('/dummy', (req, res) => {
   res.send('hello, world111!')
